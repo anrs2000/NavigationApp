9894bd51839473ceb90dbf3e54c892ee9cc20022
Ángela Ruiz <arsoto@cic.es>
2025-11-24 14:56:21 +0100

    archivos de configuracion + comienzo con las fuentes

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import "./global.css";
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./presentation/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily:{
+        'workSans-black': ['WorkSans-Black', 'sans-serif'], //sans-serif sería la fuente de respaldo
+        'workSans-light': ['WorkSans-Light'],
+        'workSans-medium': ['WorkSans-Medium'],
+      }
+    },
+  },
+  plugins: [],
+}
+
