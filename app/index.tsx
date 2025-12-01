3a599ae5a01d45fc95bd90d221513b0f7924b1e8
Ángela Ruiz <arsoto@cic.es>
2025-12-01 14:53:37 +0100

    customButton

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+
+import CustomButton from '@/components/shared/CustomButton';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import "./global.css";
 
 const App = () => {
-  return (
+  return (<>
+    <SafeAreaView></SafeAreaView>
     <View>
-      <Text className='workSans-black'>App</Text>
+      <Text className='font-workSans-black'>Ejemplo de texto utilizando la fuente con className (NativeWind)</Text>
+      <CustomButton color='primary'>PRODUCTOS</CustomButton>
     </View>
+    </>
   )
 }
 
