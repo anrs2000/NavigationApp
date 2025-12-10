1453abd13f4a959d89128f54d65f0886f9e6a194
Ángela Ruiz <arsoto@cic.es>
2025-12-10 09:11:29 +0100

    merge

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,8 @@
-<<<<<<< Updated upstream
 import React from 'react';
 import { Text, View } from 'react-native';
 
 
-import CustomButton from '@/components/shared/CustomButton';
+import CustomButton from '@/components/shared/customButton';
 import { router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import "./global.css";
@@ -21,21 +20,5 @@ const App = () => {
   )
 }
 
-=======
-import React from 'react';
 
-import { Redirect } from 'expo-router';
-import "./global.css";
-
-const App = () => {
-  return <Redirect href="/home" />
-  // return (
-  //   <View className='mt-5 mx-5'>
-  //     <Text className='workSans-black text-3xl text-primary'>App</Text>
-  //     <Text className='text-secondary-100'>Variante 100 del color secondary</Text>
-  //     <Link href="/products">ENLACE A PRODUCTOS</Link>
-  //   </View>)
-}
-
->>>>>>> Stashed changes
 export default App
